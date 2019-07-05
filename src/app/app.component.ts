c068302939e28c2193c4f5d789afb70279b9d074
Georgios Andreadis <G.Andreadis@student.tudelft.nl>
2019-07-05 23:38:11 +0200

    Fix notch and toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
 
+declare var window: any;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -20,8 +21,17 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      // this.statusBar.backgroundColorByHexString('#ffffff')
       this.statusBar.overlaysWebView(true);
       this.splashScreen.hide();
+
+      if (window.AndroidNotch) {
+        const style = document.documentElement.style;
+
+        window.AndroidNotch.getInsetTop(px => {
+          style.setProperty('--ion-safe-area-top', px + 'px');
+        }, (err) => console.error('Failed to get insets top:', err));
+      }
     });
   }
 }
