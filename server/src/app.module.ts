91880cbb2abbec879753cebea1fa896da78a8381
Georgios Andreadis <info@gandreadis.com>
2020-03-11 09:47:44 +0100

    Add serve static to server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ApiModule } from './api/api.module';
+import {ServeStaticModule} from "@nestjs/serve-static";
+import {join} from 'path';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/statistak', { useNewUrlParser: true }), ApiModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'client', 'build'),
+      exclude: ['/api*'],
+    }),
+    MongooseModule.forRoot('mongodb://localhost/statistak', {useNewUrlParser: true}),
+    ApiModule
+  ],
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule {
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(process.env.SERVER_PORT || 5000);
 }
 
 bootstrap();
