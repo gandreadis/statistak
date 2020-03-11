4801f3d11490f49e7141553fbe5e2bdb6c113a71
Georgios Andreadis <info@gandreadis.com>
2020-03-11 10:09:11 +0100

    Add HTTPS to server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import * as dotenv from 'dotenv';
+import * as fs from "fs";
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let options = {};
+
+  if (process.env.SERVER_PORT == "443") {
+    options = {
+      key: fs.readFileSync('/etc/letsencrypt/live/statistak.nl/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/statistak.nl/cert.pem'),
+    };
+  }
+
+  const app = await NestFactory.create(AppModule, options);
   app.enableCors();
   await app.listen(process.env.SERVER_PORT || 5000);
 }
