2f5c47a765c72f9cf1db37d1e178488dc7877b2e
Georgios Andreadis <info@gandreadis.com>
2022-09-15 16:23:30 +0200

    Update excel exporter

--- server/src/api/tours/tours.service.ts
+++ server/src/api/tours/tours.service.ts
@@ -134,7 +134,7 @@ export class ToursService {
           counter++,
           performance.date,
           performance.time,
-          '?',
+          performance.duration || '?',
           performance.locationName,
           performance.city,
           '',
